aacc7098f560538d5ced729a9ce644abb3d82498
Meysam Emady <m.emady68@gmail.com>
2024-01-03 10:03:13 +0330

    update

--- app/[lang]/medical/doctor/[id]/_components/medical-documents/medical-document.tsx
+++ app/[lang]/medical/doctor/[id]/_components/medical-documents/medical-document.tsx
@@ -17,7 +17,7 @@ export const MedicalDocument = ({ document }: { document : Document[] }) => {
     <div className="mt-[30px] md:mt-[60px] lg:mt-[100px]">
       <Title title="medical documents" />
       <div className=" container mt-4 md:mt-[40px] grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6">
-        {document.length && document.map((item: any) => (
+        {document.map((item: any) => (
           <DocumentItem key={item.id} {...item} />
         ))}
       </div>
