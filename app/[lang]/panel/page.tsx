11dc293fdcc0d00aec16ee6c07c0dfbd105ed775
Meysam Emady <m.emady68@gmail.com>
2024-05-12 08:51:54 +0330

    add comment

--- app/[lang]/panel/page.tsx
+++ app/[lang]/panel/page.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import UserAlert from './_components/alert';
-import { Profile } from './_components/profile';
-import { SessionProvider } from 'next-auth/react';
-import { auth } from '@/auth';
+import React from "react";
+import UserAlert from "./_components/alert";
+import { Profile } from "./_components/profile";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import { Comment } from "../components/comment/comment";
 
-const PanelPage =  async() => {
-
-    const session = await auth();
+const PanelPage = async () => {
+  const session = await auth();
 
   return (
     <div className=" container">
       <UserAlert />
-
-     
-        <Profile />
- 
+      <Profile />
+      <div className="container">
+      <Comment />
+      </div>
     </div>
   );
-}
+};
 
 export default PanelPage;
